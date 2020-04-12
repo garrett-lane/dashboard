0d1ca086df3a47fd8dee67c5901b35e2baa3eb8b
garrett-lane <45501825+garrett-lane@users.noreply.github.com>
2020-04-12 01:48:14 -0500

    Updated for Easter 2021
    Happy Easter!

--- countdowns.js
+++ countdowns.js
@@ -1,13 +1,4 @@
-var countDownDate
-var x
-var now
-var distance
-var days
-var hours
-var minutes
-var seconds
-
-// My Birthday - working
+// My Birthday
 var BcountDownDate = new Date("Oct 6, 2020").getTime();
 var Bx = setInterval(function() {
 
@@ -23,14 +14,13 @@ var Bx = setInterval(function() {
     document.getElementById("cd-birthday").innerHTML = Bdays + "d " + Bhours + "h " +
         Bminutes + "m " + Bseconds + "s ";
 
-    // If the count down is over, write some text 
     if (Bdistance < 0) {
         clearInterval(Bx);
         document.getElementById("cd-birthday").innerHTML = "Happy Birthday!";
     }
 }, 1000);
 
-// Christmas - working
+// Christmas
 var CcountDownDate = new Date("Dec 25, 2020").getTime();
 var Cx = setInterval(function() {
 
@@ -46,14 +36,13 @@ var Cx = setInterval(function() {
     document.getElementById("cd-christmas").innerHTML = Cdays + "d " + Chours + "h " +
         Cminutes + "m " + Cseconds + "s ";
 
-    // If the count down is over, write some text 
     if (Cdistance < 0) {
         clearInterval(Cx);
         document.getElementById("cd-christmas").innerHTML = "Merry Christmas!";
     }
 }, 1000);
 
-// Thanksgiving - working
+// Thanksgiving
 var TcountDownDate = new Date("Nov 26, 2020").getTime();
 var Tx = setInterval(function() {
 
@@ -69,14 +58,13 @@ var Tx = setInterval(function() {
     document.getElementById("cd-thanksgiving").innerHTML = Tdays + "d " + Thours + "h " +
         Tminutes + "m " + Tseconds + "s ";
 
-    // If the count down is over, write some text 
     if (Tdistance < 0) {
         clearInterval(Tx);
         document.getElementById("cd-thanksgiving").innerHTML = "Happy Thanksgiving!";
     }
 }, 1000);
 
-// Halloween - not working
+// Halloween
 var HcountDownDate = new Date("Oct 31, 2020").getTime();
 var Hx = setInterval(function() {
 
@@ -92,14 +80,13 @@ var Hx = setInterval(function() {
     document.getElementById("cd-halloween").innerHTML = Hdays + "d " + Hhours + "h " +
         Hminutes + "m " + Hseconds + "s ";
 
-    // If the count down is over, write some text 
     if (Hdistance < 0) {
         clearInterval(Hx);
         document.getElementById("cd-halloween").innerHTML = "Happy Spooky Day!";
     }
 }, 1000);
 
-// St. Patricks Day - working
+// St. Patricks Day
 var ScountDownDate = new Date("Mar 17, 2021").getTime();
 var Sx = setInterval(function() {
 
@@ -115,15 +102,14 @@ var Sx = setInterval(function() {
     document.getElementById("cd-stpatty").innerHTML = Sdays + "d " + Shours + "h " +
         Sminutes + "m " + Sseconds + "s ";
 
-    // If the count down is over, write some text 
     if (Sdistance < 0) {
         clearInterval(Sx);
         document.getElementById("cd-stpatty").innerHTML = "Happy St. Patricks Day!";
     }
 }, 1000);
 
-// Easter - working
-var EcountDownDate = new Date("Apr 12, 2020").getTime();
+// Easter
+var EcountDownDate = new Date("Apr 4, 2021").getTime();
 var Ex = setInterval(function() {
 
     var Enow = new Date().getTime();
@@ -138,14 +124,13 @@ var Ex = setInterval(function() {
     document.getElementById("cd-easter").innerHTML = Edays + "d " + Ehours + "h " +
         Eminutes + "m " + Eseconds + "s ";
 
-    // If the count down is over, write some text 
     if (Edistance < 0) {
         clearInterval(Ex);
-        document.getElementById("cd-easter").innerHTML = "Happy Birthday!";
+        document.getElementById("cd-easter").innerHTML = "Happy Easter!";
     }
 }, 1000);
 
-// Hack Club Anniversary - working
+// Hack Club Anniversary
 var HCcountDownDate = new Date("Sep 11, 2020").getTime();
 var HCx = setInterval(function() {
 
@@ -161,7 +146,6 @@ var HCx = setInterval(function() {
     document.getElementById("cd-hcversary").innerHTML = HCdays + "d " + HChours + "h " +
         HCminutes + "m " + HCseconds + "s ";
 
-    // If the count down is over, write some text 
     if (HCdistance < 0) {
         clearInterval(HCx);
         document.getElementById("cd-hcversary").innerHTML = "Happy HC Anniversary!";
